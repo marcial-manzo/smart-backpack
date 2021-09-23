ba03f221637787a85bc55daa2510d17d401fd5de
Marcial Manzo <manzomarcial@gmail.com>
2021-09-23 20:39:48 -0300

    uncomments rfid reading feature

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>
-//#include <MFRC522.h>
+#include <MFRC522.h>
 
-#define RST_PIN 9          // Configurable, see typical pin layout above
-#define SS_PIN  10         // Configurable, see typical pin layout above
+#define RST_PIN 9
+#define SS_PIN  10
 
-//MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 #include "pitches.h" 
 #define REST      0 
 int buzzer = 5; 
@@ -67,12 +67,12 @@ void offLedRechazar(){
 }
 void setup() {
   ledSetup();
-  Serial.begin(9600);		// Initialize serial communications with the PC
-  while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();			// Init SPI bus
-//  mfrc522.PCD_Init();		// Init MFRC522
-  delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
-//  mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+  Serial.begin(9600);
+  while (!Serial);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  delay(4);
+  mfrc522.PCD_DumpVersionToSerial();
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
@@ -92,7 +92,7 @@ void loop() {
 
 bool isNewCardAvailable() {
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-/*  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return false;
   }
 
@@ -100,13 +100,13 @@ bool isNewCardAvailable() {
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return false;
   }
-*/
+
   return false;
 }
 
 String readUID() {
   String uid = "";
-/*
+
   for (byte i = 0; i < mfrc522.uid.size; i++)
   {
     uid.concat(String(mfrc522.uid.uidByte[i], HEX));
@@ -116,7 +116,7 @@ String readUID() {
   mfrc522.PICC_HaltA(); 
 
   Serial.print(F("uid: "));
-  Serial.println(uid);*/
+  Serial.println(uid);
 
   return uid;
 }
